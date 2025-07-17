6daa030162e34ba3edb36f7c433997457bf09193
OutOfIQ <147145087+OutOfIQgg@users.noreply.github.com>
2025-07-17 22:15:00 +0300

    Update trrng.lua

--- trrng.lua
+++ trrng.lua
@@ -1,56 +1,8 @@
 local love = require("love") -- needed if running standalone, ignore if running in love
 
 local trng = {}
-trng.a = {} -- a = advanced (MOST ACCURATE AND EFFICIENT)
 
--- Your cf function (time-based toggle)
-trng.a.cf = function(bool)
-    local t = love.timer.getTime()
-    local cal = math.sin(t * 10000) + math.cos(t * 0.7)
-    return cal % 2 > 1 and not bool or bool
-end
-
--- Improved grn function
-trng.a.grn = function(min, max, mloops)
-    local gn = love.math.random(min, max)        -- random start
-    local going_up = love.math.random() > 0.5    -- random initial direction
-    local loops = 0
-
-    mloops = (type(mloops) == "number") and mloops or 1000
-
-    while loops < mloops do
-        loops = loops + 1
-
-        if going_up then
-            gn = gn + 1
-            if gn >= max then
-                going_up = false
-            end
-        else
-            gn = gn - 1
-            if gn <= min then
-                going_up = true
-            end
-        end
-
-        -- Add chance to break out early:
-        -- combine your cf with some random chance
-        local cf_res = trng.a.cf(love.math.random() > 0.5)
-        local rand_chance = love.math.random() > 0.8 -- 20% chance to break
-
-        if not cf_res or rand_chance then
-            break
-        end
-    end
-
-    -- Clamp just in case
-    gn = math.max(min, math.min(max, gn))
-    return gn
-end
-
-trng.b ={} -- b = basic (LEAST ACCURATE)
-
-trng.b.cf = function (bool)
+trng.cf = function (bool)
     local gr = love.math.random(1, 10)
     if gr <= 5 then
         return bool == false
@@ -60,7 +12,7 @@ trng.b.cf = function (bool)
     end
 end
 
-trng.b.grn = function (min, max, mloops)
+trng.grn = function (min, max, mloops)
     local gn = love.math.random(min, max) + 5
     local count = true
     local loops = 0
@@ -96,44 +48,6 @@ trng.b.grn = function (min, max, mloops)
             return math.floor(gn)
         end
     end
-end
-
-trng.e = {} -- e = experimental (FULL OF EXPERMENTS, DON'T RELY ON IT)
-
-trng.e.cf = function (bool)
-    local bool1 = false
-    local dcin = 1
-    local mloops = 10 / (love.timer.getDelta() + 0.2)
-    local loops = 0
-    local dci = {true, false}
-
-    while loops < mloops do
-        loops = loops + 1
-        if bool1 == false then
-            dcin = dcin + 1
-            bool1 = true
-        elseif bool1 == true then
-            dcin = dcin - 1
-            bool1 = false
-        end
-    end
-
-    return bool == dci[dcin]
-end
-
-trng.e.grn = function(min, max, mloops)
-    local range = max - min + 1
-    if type(mloops) ~= "number" then
-        mloops = 250
-    else
-        local dt = love.timer.getDelta() + 0.2
-        local t = love.timer.getTime() + dt
-        mloops = (max / dt) % max * t
-        mloops = math.floor(mloops)
-    end
-
-    local offset = mloops % range
-    return min + offset
 end
 
 return trng
